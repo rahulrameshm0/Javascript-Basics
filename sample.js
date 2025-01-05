5a9a986f61e2c8556bf52a4e8d4619d5e6d7cc14
rahulrameshm0 <iamrahulm23@gmail.com>
2025-01-05 19:32:03 +0530

    learned basic element types in javascript

--- sample.js
+++ sample.js
@@ -2,3 +2,22 @@
 
 var a = document.getElementById("head1");
 a.style.color = "green";
+
+
+// Get Element By Name
+
+var b = document.getElementsByClassName("head1");
+// b[1].innerHTML = "Hello";
+b[1].style.color = "red";
+
+var c = document.getElementsByTagName("h1");
+c[0].style.color = "blue";
+
+// Get Element By Name  
+
+var d = document.getElementsByName("text")[0];
+var e = document.getElementById("head");
+
+function message(){
+    e.innerHTML = "Hello " + d.value;  
+}
